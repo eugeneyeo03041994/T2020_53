a1f806341616cd5758f05e7066d4ed6d780f2fbb
Eugene Yeo <eugeneyeo1994@gmail.com>
2020-01-07 13:16:07 +0800

    added ui container and menu bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-
 import "semantic-ui-css/semantic.css";
 
 class App extends React.Component {
@@ -14,9 +13,28 @@ class App extends React.Component {
 
     render() {
         return (
+            <body>
+
             <div className="ui container">
+
+                <div className="ui top menu">
+                    <div className="item">
+                        <img src="/images/logo.png"></img>
+                    </div>
+                    <a className="item float right">Features</a>
+                    <a className="item">Testimonials</a>
+                    <a className="item">Log in</a>
+
+                </div>
+
+                <h1 className="ui dividing header header-style">
+                    Title here
+                    <h2 className="ui sub header">
+                        sub title intruduction
+                    </h2>
+                </h1>
                 <header>
-                    <img src={logo} alt="logo" />
+
                     <p>
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
@@ -28,9 +46,11 @@ class App extends React.Component {
                         {this.state.someValue}
                     </a>
 
-                    <MyComponent/>
                 </header>
             </div>
+
+            </body>
+
         );
     }
 }
