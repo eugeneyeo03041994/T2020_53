20fd342ecec4aadcd12b070e9123008b29bf36b0
Eugene Yeo <eugeneyeo1994@gmail.com>
2020-01-07 12:03:19 +0800

    added semantic ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "semantic-ui-css/semantic.css";
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            someValue: 300
+        };
+        setTimeout(() => this.setState({someValue: 1000}), 5000);
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <header>
+                    <img src={logo} alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {this.state.someValue}
+                    </a>
+
+                    <MyComponent/>
+                </header>
+            </div>
+        );
+    }
 }
+
+const MyComponent = () => (
+    <h1 className="ui dividing header">
+      Hello there
+    </h1>
+);
 
 export default App;
